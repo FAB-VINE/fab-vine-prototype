21604964f3a99ba07686289ccf9443539e3d23e8
IsmaLB <61549609+Isma-LB@users.noreply.github.com>
2026-07-08 12:45:35 -0500

    feat: add pin solder test script

---
+++ NODE-LOGIC/test-scripts/pcb-back-pins-test/pcb-back-pins-test.ino
@@ -0,0 +1,18 @@
+
+
+void setup() {
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
+    digitalWrite(7, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
